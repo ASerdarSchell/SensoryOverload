8036aa72552fee867d60275b9e440c42fa76726d
ASerdarSchell <adam@schellgames.com>
2015-05-02 11:49:22 -0400

    Updated UI to work of Prefabs

---
+++ unity/Assets/Scripts/StoryNodes/InterstitialNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterstitialNode : StoryNode {
+
+	public Sprite Image = null;
+	public string DialogueText = "";
+	public StoryNode NextNode = null;
+	
+	override public void Display()
+	{
+		UIManager.Instance.InterstitialUI.DialogueText.text = DialogueText;
+		UIManager.Instance.InterstitialUI.DisplayImage.sprite = Image;
+		
+		UIManager.Instance.InterstitialUI.gameObject.SetActive(true);
+		
+		UIManager.Instance.InterstitialUI.NextButton.onClick.AddListener (OnClick);
+	}
+	
+	override public void Dismiss()
+	{
+		UIManager.Instance.InterstitialUI.gameObject.SetActive(false);
+	}
+	
+	public void OnClick()
+	{
+		StoryManager.Instance.ShowNode (NextNode);
+	}
+}
---
+++ unity/Assets/Scripts/UserInterface/UIInterstitialPanel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIInterstitialPanel : MonoBehaviour {
+
+	public Button NextButton;
+	public Text DialogueText;
+	public Image DisplayImage;
+}
