585901f6b251e4910b1f2856e7d0a824042a8d21
ASerdarSchell <adam@schellgames.com>
2015-05-02 22:36:28 -0400

    bug fix for CameraLerp

--- unity/Assets/Scripts/StoryNodes/CameraLerpNode.cs
+++ unity/Assets/Scripts/StoryNodes/CameraLerpNode.cs
@@ -33,20 +33,27 @@ public class CameraLerpNode : CameraNode {
 		Quaternion StartRot = Camera.main.transform.rotation;
 		float StartTime = Time.time;
 
-		while (StartTime + timeToLerp < Time.time) {
+		Debug.Log ("Time: " + StartTime);
+
+		while (StartTime + timeToLerp > Time.time) {
 			float percent = (Time.time - StartTime) / timeToLerp;
 			Camera.main.transform.rotation = Quaternion.Lerp(StartRot, CameraOrientation.transform.rotation, percent);
 			Camera.main.transform.position = Vector3.Lerp (StartPos, CameraOrientation.transform.position, percent);
+			Debug.Log(StartPos.ToString() + " " + CameraOrientation.transform.position.ToString() + " " + percent.ToString() + " " + Vector3.Lerp (StartPos, CameraOrientation.transform.position, percent));
 			yield return null;
 		}
 		StartTime = Time.time;
 
-		while (StartTime + timeToLerp < Time.time) {
+		Debug.Log ("Time: " + StartTime);
+
+		while (StartTime + timeToLerp > Time.time) {
 			float percent = (Time.time - StartTime) / timeToLerp;
 			Camera.main.transform.rotation = Quaternion.Lerp(CameraOrientation.transform.rotation, StartRot, percent);
 			Camera.main.transform.position = Vector3.Lerp (CameraOrientation.transform.position, StartPos, percent);
 			yield return null;
 		}
+
+		Debug.Log ("Time: " + Time.time);
 
 		if (clearOnComplete)
 			StoryManager.Instance.ShowNode (NextNode);
